4a7ea5efdedc7a014dea907e62cda721377d3ee8
oh.kwon <slash512@gmail.com>
2019-03-14 01:08:04 +0900

    learn to machine learning with tensorflow step 1. - linear regression

---
+++ Linear Regression/computational_graph.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+
+node1 = tf.constant(3.0, tf.float32)
+node2 = tf.constant(4.0) # also tf.float32 implicitly
+node3 = tf.add(node1, node2)
+
+print("node1:", node1, "node2:", node2)
+print("node3:", node3)
+
+sess = tf.Session()
+
+print("sess.run(node1, node2): ", sess.run([node1, node2]))
+print("sess.run(node3): ", sess.run(node3))
